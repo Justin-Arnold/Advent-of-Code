731c4edc728e45565f0b659dfc226bd67b3d4fc9
Justin Arnold <justin.arnold@programmer.net>
2023-12-03 16:18:50 -0500

    update package to handle new structure

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ let package = Package(
     name: "AdventOfCode",
     products: [
         .executable(
-            name: "aoc",
-            targets: ["Puzzles"]),
-        .executable(
             name: "aoc-2022",
             targets: ["2022"]),
         .executable(
@@ -16,17 +13,10 @@ let package = Package(
     ],
     targets: [
         .executableTarget(
-            name: "Puzzles",
-            path: "Sources/Puzzles"),
-        .executableTarget(
             name: "2022",
             path: "Sources/2022"),
         .executableTarget(
             name: "2023",
             path: "Sources/2023"),
-        .testTarget(
-            name: "Tests",
-            dependencies: ["Puzzles"],
-            path: "Tests")
     ]
 )
