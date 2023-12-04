70af11cd037607a9de623a92245348c144690609
Justin Arnold <justin.arnold@programmer.net>
2023-12-03 19:30:23 -0500

    fixed and cached 2023 d1 p2

--- Sources/2023/Day1Solutions.swift
+++ Sources/2023/Day1Solutions.swift
@@ -21,39 +21,33 @@ struct Day1_2023: DayChallenge {
     }
     static func partTwo(input: String) -> String {
         let replacements = ["one": "o1ne", "two": "t2wo", "three": "t3hree", "four": "f4our", "five": "f5ive", "six": "s6ix", "seven": "s7even", "eight": "e8ight", "nine": "n9ine"]
-        do {
-            // let fileContents = try String(contentsOf: file1URL, encoding: .utf8)
-            let lines = input.split(separator: "\n")
-            var totalSum = 0
+        // let fileContents = try String(contentsOf: file1URL, encoding: .utf8)
+        let lines = input.split(separator: "\n")
+        var totalSum = 0
 
-            for line in lines {
+        for line in lines {
 
-                var modifiedLine = String(line)
-                for replacement in replacements {
-                    modifiedLine = modifiedLine.replacingOccurrences(of: replacement.key, with: replacement.value)
-                }
-                modifiedLine = modifiedLine.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+            var modifiedLine = String(line)
+            for replacement in replacements {
+                modifiedLine = modifiedLine.replacingOccurrences(of: replacement.key, with: replacement.value)
+            }
+            modifiedLine = modifiedLine.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
 
-                if !modifiedLine.isEmpty {
-                    let firstIndex = modifiedLine.startIndex
-                    let lastIndex = modifiedLine.index(before: modifiedLine.endIndex)
+            if !modifiedLine.isEmpty {
+                let firstIndex = modifiedLine.startIndex
+                let lastIndex = modifiedLine.index(before: modifiedLine.endIndex)
 
-                    let first = String(modifiedLine[firstIndex])
-                    let last = String(modifiedLine[lastIndex])
+                let first = String(modifiedLine[firstIndex])
+                let last = String(modifiedLine[lastIndex])
 
-                    if modifiedLine.count == 1 {
-                        totalSum += Int(first + first) ?? 0
-                    } else {
-                        totalSum += Int(first + last) ?? 0
-                    }
+                if modifiedLine.count == 1 {
+                    totalSum += Int(first + first) ?? 0
+                } else {
+                    totalSum += Int(first + last) ?? 0
                 }
             }
-            print(totalSum)
-            return String(totalSum)
-        } catch {
-            print("Error reading the file: \(error)")
-            return "Failed"
         }
+        return String(totalSum)
     }
 }
 
