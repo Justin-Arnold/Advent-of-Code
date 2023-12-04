4197ff179369df0fe1f68a26490122d6286ef99e
Justin Arnold <justin.arnold@programmer.net>
2023-12-03 21:16:58 -0500

    solve 2025 d1 pt1 and p2

--- Sources/2015/Day1Solutions.swift
+++ Sources/2015/Day1Solutions.swift
@@ -3,10 +3,40 @@
 
     struct Day1_2015: DayChallenge {
         static func partOne(input: String) -> String {
-            return input
+            var floor = 0
+            //for each character in input
+            for (_, character) in input.enumerated() {
+                //if character is (
+                if character == "(" {
+                    //floor += 1
+                    floor += 1
+                } else if character == ")" {
+                    //floor -= 1
+                    floor -= 1
+                }
+
+            }
+
+            return String(floor)
         }
         static func partTwo(input: String) -> String {
-            return input
+            var floor = 0
+            //for each character in input
+            for (index, character) in input.enumerated() {
+                //if character is (
+                if character == "(" {
+                    //floor += 1
+                    floor += 1
+                } else if character == ")" {
+                    //floor -= 1
+                    floor -= 1
+                }
+
+                if floor == -1 {
+                    return String(index + 1)
+                }
+            }
+            return String(floor)
         }
     }
 
